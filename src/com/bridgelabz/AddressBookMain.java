27e0734c954a945ef4ee87563238b1df58081c08
GeetaSMath <geetasmath@gmail.com>
2021-12-03 10:16:08 +0530

    Added:addressbook annotation csv and json

---
+++ src/com/bridgelabz/AddressBookMain.java
@@ -0,0 +1,18 @@
+package com.bridgelabz;
+
+import java.io.IOException;
+import java.text.ParseException;
+
+
+public class AddressBookMain {
+    public static void main(String[] args) throws ParseException, IOException {
+        AddressBookManager makeBook = new AddressBookManager();
+        makeBook.takeOption();
+        boolean condition =true;
+        while (condition){
+            boolean monitorUser = makeBook.takeOption();
+            if (monitorUser)
+                break;
+        }
+    }
+}
